f7c2f3eea287043b3a677b53000f96a12bf3ef83
kr <karan1980gmail.com>
2015-02-01 22:14:49 +0100

    Everything

---
+++ src/main/java/com/companies/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.companies.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class HelloWorldController {
+ 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ModelAndView hello() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("hello");
+         
+        String str = "Hello World!";
+        mav.addObject("message", str);
+ 
+        return mav;
+    }
+ 
+}
